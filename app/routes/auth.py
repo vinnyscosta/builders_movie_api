ef2786976a27ece5d250042dadd66c84225e354d
vinnyscosta <vinicius00.vc@gmail.com>
2025-05-27 21:22:28 -0300

    feat: rota de criação de token e funções de criação e verificação

---
+++ app/routes/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from app.config import (
+    SECRET_KEY,
+    ALGORITHM,
+)
+
+# Define autenticação com Bearer Token
+security = HTTPBearer()
+
+# Criação do router
+router = APIRouter()
+
+
+# Função para criar o token
+def create_access_token(expires_delta: int = 30):
+    """Cria um token de acesso para o usuario"""
+    to_encode = {
+        "exp": datetime.utcnow() + (timedelta(minutes=expires_delta)),
+        "sub": "generic_user"
+    }
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+# Função para verificar o token
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):  # noqa: E501
+    """Verifica se o token utilizado ainda é valido"""
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido ou expirado"
+        )
+
+
+# Rota para gerar o token
+@router.post("/get-token")
+def generate_token():
+    access_token = create_access_token()
+    return {"access_token": access_token, "token_type": "Bearer"}
